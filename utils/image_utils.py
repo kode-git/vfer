c61598c77af74537895da960aa524cdfd3fd012d
kode-git <mariosessa64@gmail.com>
2022-04-10 12:48:50 +0200

    Adding the format function for ImageWorker

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -2,36 +2,44 @@
 
 import cv2
 import os
+import glob
 
 
-class ImageResizer():
+class ImageWorker():
 
     def __init__(self) -> None:
          pass
 
-    def resize_image(self, path, filename, dest, width, height):
-    
-        img = cv2.imread(str.join(path, filename), cv2.IMREAD_UNCHANGED)
-        print('Original Dimensions : ',img.shape)
+    def format_converter(self, path, format, dest_path):
+        count = 0
+        os.mkdir(dest_path)
+        for file in glob.glob(path + "/*." + format):
+            img = cv2.imread(file, cv2.IMREAD_UNCHANGED)
+            resized = cv2.resize(img, (224,224), interpolation=cv2.INTER_CUBIC)
+            print('Resized writing for', dest_path + "resized_" + str(count) + "." + format )
+            cv2.imwrite(dest_path + "resized_" + str(count) + "." + format, resized)
+            count += 1
 
-        # new dimensions
-        dim = (width, height)
-  
-        # resize image
-        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    
-        print('Resized Dimensions : ', resized.shape)
-    
-        cv2.imwrite(os.path.join(dest , filename), img)
-        cv2.waitKey(0)
+        
 
 
-    def navigate_path(path):
-        pass
+    def navigate_path(self, path):
+        count = 0
+        for dir in os.listdir(path):
+            if os.path.isfile(os.path.join(path, dir)):
+                count += 1
+        print('Counter for', path, ':', count)
+        return count
 
 
     def setup():
         pass
 
 if __name__ == "__main__":
-    pass
+    iw = ImageWorker()
+    origin_path = input("Tell the origin path: ")
+    dest_path = input("Tell the destination path: ")
+    format = input("Tell the images format: ")
+    iw.format_converter(origin_path, format, dest_path)
+
+
